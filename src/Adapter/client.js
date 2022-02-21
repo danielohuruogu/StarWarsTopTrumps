4a83563be004b5b38644963d5cef775c583476d5
Daniel Ohuruogu <daniel.ohuruogu@hotmail.co.uk>
2022-02-21 17:36:23 +0000

    app initialisation, create components and adapter file. create inital fetch of data from pages. comment out attempt at scraping all the info from the pages

---
+++ src/Adapter/client.js
@@ -0,0 +1,48 @@
+import fetch from "unfetch";
+
+const checkStatus = response => {
+    if (response.ok) {
+        return response
+    }
+    const error = new Error(response.statusText);
+    error.response = response;
+    return Promise.reject(error);
+}
+
+
+
+
+export const getAllCharacters = () => {
+
+    fetch("https://swapi.dev/api/people/")
+        .then(checkStatus);
+    // console.log(response)
+    // var data = JSON.stringify(response, null, 2)
+
+    // allCharacters = allCharacters + data.results
+    // console.log(data)
+    // console.log(allCharacters)
+
+    // while (data.next != null) {
+    //     console.log("grabbing data...")
+    //     response = fetch(data.next)
+    //     var data = JSON.stringify(response, null, 2)
+    //     allCharacters = allCharacters + data.results
+    // }
+    // console.log("All characters have been collected")
+
+    // return allCharacters
+}
+    // .then((result) => {
+    //     console.log(result);
+    //     return result.nextPage();
+    // })
+    // .then((result) => {
+    //     console.log(result);
+    //     return result.previousPage();
+    // }).then((result)=> {
+    //     console.log(result);
+    // })
+    // .catch((err)=> {
+    //     console.log(err);
+    // })
--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import { getAllCharacters } from './Adapter/client.js'
 
 function App() {
+
+    let url = 'https://swapi.dev/api/people/'
+
+    const fetchData = async () => {
+        const response = await fetch(url)
+        const data = await response.json()
+    }
+
+    useEffect(()=>{
+        fetchData();
+    }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+
     </div>
   );
 }
 
 export default App;
+
+    // const grabData = () => {
+    //     getAllCharacters()
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setServerData(data)
+    //     }).catch(err => {
+    //     console.log(err.response)
+    //     err.response.json().then(res=> {
+    //       console.log(res)
+    //     })
+    // })
+  // }
+  // const [serverData, setServerData] = useState([])
+      // grabData();
